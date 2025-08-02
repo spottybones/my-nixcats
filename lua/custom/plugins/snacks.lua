a6c268106089756d56a82a1fb648ec2167afbe38
Scott Burns <scott@lentigo.net>
2025-08-02 12:09:20 -0400

    fix(snacks): replace 'config' function in spec with 'keys' attribute
    The 'config' key in the prior version apparently disabled lazy.nvim's
    plugin loader from automatically running setup() functions for the
    submodules. The function was only setting up keymaps for me so I
    converted the maps to the 'keys' attribute. Once that was done the
    plugin was initialized properly.

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,7 +1,6 @@
 -- Enable Snacks library
 return {
   'folke/snacks.nvim',
-  event = 'VimEnter',
   lazy = false,
   priority = 1000,
   auto_install = require('nixCatsUtils').lazyAdd(true, false),
@@ -13,62 +12,43 @@ return {
     bigfile = { enabled = true },
     indent = { enabled = true },
     quickfile = { enabled = true },
+    explorer = { enabled = true },
+    dashboard = { enabled = true },
+    image = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
   },
 
-  config = function()
-    local builtin = require 'snacks.picker'
-    vim.keymap.set('n', '<leader>sh', function()
-      builtin.help()
-    end, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', function()
-      Snacks.picker.keymaps()
-    end, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', function()
-      Snacks.picker.files()
-    end, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', function()
-      Snacks.picker()
-    end, { desc = '[S]earch [S]elect Pickers' })
-    vim.keymap.set('n', '<leader>sw', function()
-      Snacks.picker.grep_word()
-    end, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', function()
-      Snacks.picker.grep()
-    end, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', function()
-      Snacks.picker.diagnostics()
-    end, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', function()
-      Snacks.picker.resume()
-    end, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', function()
-      Snacks.picker.recent()
-    end, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', function()
-      Snacks.picker.buffers()
-    end, { desc = '[ ] Find existing buffers' })
+  -- stylua: ignore
+  keys = {
+    { '<leader>sh', function() Snacks.picker.help() end, desc = '[S]earch [H]elp', },
+    { '<leader>sk', function() Snacks.picker.keymaps() end, desc = '[S]earch [K]eymaps', },
+    { '<leader>sf', function() Snacks.picker.files() end, desc = '[S]earch [F]iles', },
+    { '<leader>ss', function() Snacks.picker() end, desc = '[S]earch [S]elect Pickers', },
+    { '<leader>sw', function() Snacks.picker.grep_word() end, desc = '[S]earch current [W]ord', },
+    { '<leader>sg', function() Snacks.picker.grep() end, desc = '[S]earch by [G]rep', },
+    { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = '[S]earch [D]iagnostics', },
+    { '<leader>sr', function() Snacks.picker.resume() end, desc = '[S]earch [R]esume', },
+    { '<leader>s.', function() Snacks.picker.recent() end, desc = '[S]earch Recent Files ("." for repeat)', },
+    { '<leader><leader>', function() Snacks.picker.buffers() end, desc = '[ ] Find existing buffers', },
 
-    vim.keymap.set('n', '<leader>/', function()
-      Snacks.picker.lines()
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    { '<leader>/', function() Snacks.picker.lines() end, desc = '[/] Fuzzily search in current buffer', },
 
-    vim.keymap.set('n', '<leader>s/', function()
-      Snacks.picker.grep_buffers()
-    end, { desc = '[S]earch [/] in Open Files' })
+    { '<leader>s/', function() Snacks.picker.grep_buffers() end, desc = '[S]earch [/] in Open Files', },
 
     -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set('n', '<leader>sn', function()
+    { '<leader>sn', function()
       Snacks.picker.files { cwd = vim.fn.stdpath 'config', title = 'Search Neovim Config Files' }
-    end, { desc = '[S]earch [N]eovim files' })
+    end, desc = '[S]earch [N]eovim files', },
 
     -- open up LazyGit
-    vim.keymap.set('n', '<leader>gg', function()
-      Snacks.lazygit()
-    end, { desc = '[G] Open lazy[G]it' })
+    { '<leader>gg', function() Snacks.lazygit() end, desc = '[G] Open lazy[G]it', },
 
     -- open the file explorer
-    vim.keymap.set('n', '\\', function()
-      Snacks.explorer.open()
-    end, { desc = '[\\] Open File Explorer' })
-  end,
+    { '\\', function() Snacks.explorer.open() end, desc = '[\\] Open File Explorer', },
+  },
 }
