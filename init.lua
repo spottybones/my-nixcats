5e968468beb5dcdf94cad6169a5f521441e256ff
Scott Burns <scott@lentigo.net>
2025-08-04 12:10:34 -0400

    feat(which-key): move keymap list to right side (helix)

--- init.lua
+++ init.lua
@@ -323,7 +323,9 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      require('which-key').setup {
+        preset = 'helix',
+      }
 
       -- Document existing key chains
       require('which-key').add {
